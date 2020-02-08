c9d7df48b7d4d36519825e4db648ea190a3e574e
Nick Todts <nick-todts@hotmail.com>
2020-02-08 19:58:06 +0100

    Add seperate python file for putting googly eyes on a single image.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from detect_blur_image import detect_blur_image
 from detect_blur_video import detect_blur_video
 from googly_eyes import googly_eyes
+from googly_eyes_image import googly_eyes_image
 
 
 def menu():
@@ -8,16 +9,17 @@ def menu():
 ---- Detect and blur faces -----
 | 0: Detect and blur an image. |
 | 1: Detect and blur a video.  |
-| 2: Googly eyes.              |
-| 3: exit                      |
+| 2: Googly eyes w/ video.     |
+| 3: Googly eyes w/ photo.     |
+| 4: exit                      |
 |------------------------------|\n''')
     return choice
 
 
 def main():
     while True:
-        #choice = menu()
-        choice = '2'
+        choice = menu()
+        # choice = '3'
         # Execute chosen menu option.
         if choice == '0':
             detect_blur_image()
@@ -29,6 +31,9 @@ def main():
             googly_eyes()
             break
         elif choice == '3':
+            googly_eyes_image()
+            break
+        elif choice == '4':
             print("Exiting program")
             break
         else:
